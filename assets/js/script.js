f5ad28bc19f913148565dd306cd0402b1954dabc
TaimurHasan <taimurhasan11@gmail.com>
2022-03-19 12:58:26 -0400

    added styling to time slots and added class jquery selector for textbox

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,11 @@ var today = moment().format("dddd, MMMM Do");
 // find p element in header and add current day to it
 $("#currentDay")
     .text(today);
+
+$(".textBox").on("click", function(){
+    var text = $(this)
+        .text()
+        .trim()
+
+    console.log(text);
+})
